14b11b2c0ec694daee2277fa7d1a4238bf657426
Masuda-Jion <[masuda0311jion@gmail.com]>
2023-03-06 11:30:33 +0000

    [7a-start]

--- rails6-bookers2-debug-ruby3-main/app/javascript/packs/application.js
+++ rails6-bookers2-debug-ruby3-main/app/javascript/packs/application.js
@@ -2,8 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require jquery
-//= require jquery.raty.js
+
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -14,12 +13,7 @@ import "popper.js"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application" 
-import Raty from "raty.js"
-window.raty = function(elem,opt) {
-  let raty =  new Raty(elem,opt)
-  raty.init();
-  return raty;
-}
+
 
 
 Rails.start()
