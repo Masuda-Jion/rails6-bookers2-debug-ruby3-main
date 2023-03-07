75aaf37fc8407e60c31f0f423363611b51220f25
Masuda-Jion <[masuda0311jion@gmail.com]>
2023-03-07 17:35:12 +0000

    [9a-Finish]

--- rails6-bookers2-debug-ruby3-main/app/controllers/books_controller.rb
+++ rails6-bookers2-debug-ruby3-main/app/controllers/books_controller.rb
@@ -5,10 +5,10 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book_new = Book.new
     @book_comment = BookComment.new
-    @book_detail = Book.find(params[:id])
-    unless ViewCount.find_by(user_id: current_user.id, book_id: @book_detail.id)
-      current_user.view_counts.create(book_id: @book_detail.id)
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book.id)
+      current_user.view_counts.create(book_id: @book.id)
     end
+    @book_comment = BookComment.new
   end
 
   def index
